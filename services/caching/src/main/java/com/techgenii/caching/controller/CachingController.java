629401ea5e4a915a978954ff248407ef3f231681
maslakechishty <74719064+maslakechishty@users.noreply.github.com>
2020-12-09 09:57:19 +0530

    removed deal code and swagger ui added

--- services/caching/src/main/java/com/techgenii/caching/controller/CachingController.java
+++ services/caching/src/main/java/com/techgenii/caching/controller/CachingController.java
@@ -1,9 +1,12 @@
 package com.techgenii.caching.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,16 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/caching")
+@Slf4j
 public class CachingController {
 
     @Autowired
     private HazelcastInstance hazelcastInstance;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @PostMapping("/restart")
-    public ResponseEntity restartCaching() {
-        final Config config = hazelcastInstance.getConfig();
+    public ResponseEntity restartCaching(Config config) throws JsonProcessingException {
+        log.info("Restarting hazleCast with config {}",objectMapper.writeValueAsString(config));
+        final Config existingConfig = hazelcastInstance.getConfig();
         hazelcastInstance.shutdown();
-        hazelcastInstance = Hazelcast.newHazelcastInstance(config);
+        hazelcastInstance = Hazelcast.newHazelcastInstance(existingConfig);
         return ResponseEntity.ok().build();
     }
 }
